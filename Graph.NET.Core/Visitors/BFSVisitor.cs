34cd87f9274fd248e7c62db7bb3b7724960e7481
Ziocash <31541164+Ziocash@users.noreply.github.com>
2022-06-29 17:49:56 +0200

    Visitors implementation
    [+] Added DFS implementation
    [+] Added BFS implementation

---
+++ Graph.NET.Core/Visitors/BFSVisitor.cs
@@ -0,0 +1,45 @@
+﻿using Graph.NET.Core.Models;
+using Graph.NET.Core.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph.NET.Core.Visitors
+{
+    public class BFSVisitor : IVisitor
+    {
+        public string? Visited { get; private set; }
+
+        public void Visit<TValue>(IUndirectedGraph<TValue> graph)
+        {
+            List<IVertex<TValue>> parents = new();
+            Queue<IVertex<TValue>> queue = new();
+            StringBuilder stringBuilder = new();
+            graph.Root.Color = VertexColor.Gray;
+            queue.Enqueue(graph.Root);
+            while(queue.Count > 0)
+            {
+                var head = queue.ElementAt(0);
+                stringBuilder.AppendLine($"Visiting node {head.Name}");
+                foreach (var edge in graph.Edges)
+                {
+                    if (edge.Source.Name == head.Name && edge.Destination.Color == VertexColor.White)
+                    {   
+                        edge.Destination.Color = VertexColor.Gray;
+                        if(!parents.Contains(head))
+                            parents.Add(head);
+                        queue.Enqueue(edge.Destination);
+                        stringBuilder.AppendLine($"Visited node {edge.Source.Name}, has adjacent vertex {edge.Destination.Name}.");
+                    }
+                }
+                var vertex = queue.Dequeue();
+                vertex.Color = VertexColor.Black;
+                stringBuilder.AppendLine($"Visited node {vertex.Name}, no unvisited vertices near.");
+            }
+
+            Visited = stringBuilder.ToString();
+        }
+    }
+}
---
+++ Graph.NET.Core/Visitors/DFSVisitor.cs
@@ -0,0 +1,43 @@
+﻿using Graph.NET.Core.Models;
+using Graph.NET.Core.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph.NET.Core.Visitors
+{
+    public class DFSVisitor : IVisitor
+    {
+        public string? Visited { get; private set; }
+
+        public void Visit<TValue>(IUndirectedGraph<TValue> graph)
+        {
+            Stack<IVertex<TValue>> stack = new();
+            List<IVertex<TValue>> parents = new();
+            StringBuilder stringBuilder = new();
+            graph.Root.Color = VertexColor.Gray;
+            stack.Push(graph.Root);
+            while (stack.Count > 0)
+            {
+                stringBuilder.AppendLine($"Visiting node {stack.Peek().Name}");
+                foreach (var edge in graph.Edges)
+                {
+                    if ((edge.Source.Name == stack.Peek().Name || edge.Destination.Name == stack.Peek().Name) && edge.Destination.Color == VertexColor.White)
+                    {
+                        edge.Destination.Color = VertexColor.Gray;
+                        if (!parents.Contains(stack.Peek()))
+                            parents.Add(stack.Peek());
+                        stack.Push(edge.Destination);
+                        stringBuilder.AppendLine($"Has adjacent vertex {edge.Destination.Name}.");
+                    }
+                }
+                var vertex = stack.Pop();
+                vertex.Color = VertexColor.Black;
+                stringBuilder.AppendLine($"No unvisited vertices near.");
+            }
+            Visited = stringBuilder.ToString();
+        }
+    }
+}
